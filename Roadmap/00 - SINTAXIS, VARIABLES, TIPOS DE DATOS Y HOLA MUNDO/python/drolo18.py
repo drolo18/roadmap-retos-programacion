05ea61815eee167e1e4e50df2333428aed72d225
Pedro Vélez <pedrolo.pv@gmail.com>
2025-01-12 22:48:52 +0100

    Create drolo18.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/drolo18.py
@@ -0,0 +1,33 @@
+## https://.python.org/
+
+## comentario en una linea
+
+
+"""
+esto es un comentario  
+en varias líneas 
+también
+"""
+
+'''
+esto también es
+un comantario en varias 
+líneas
+'''
+
+my_variable = "Esto es mi variable"
+my_variable = "Nueva variable"
+
+MY_CONSTANT = "Esto es mi constante"
+
+my_int = 25
+
+my_float = 2.5
+
+my_bool = False
+
+my_str = "mi cadena de texto"
+
+my_other_ str = "mi otra cadena de texto"
+
+print ("Hola, python!")
